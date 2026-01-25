ebc6d04fa59c73aa02a58b5643f2e4c4c189a748
mangoyi <mangoyiy@163.com>
2026-01-26 00:43:18 +0800

    feat: ✨ 一个简单完整RAG应用

---
+++ main.py
@@ -0,0 +1,56 @@
+import bs4
+from langchain_classic import hub
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import Chroma
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+#### INDEXING ####
+loader = WebBaseLoader(
+    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+    bs_kwargs=dict(
+        parse_only=bs4.SoupStrainer(
+            class_=("post-content", "post-title", "post-header")
+        )
+    ),
+)
+docs = loader.load()
+
+# Split
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+splits = text_splitter.split_documents(docs)
+
+# Embed
+vectorstore = Chroma.from_documents(documents=splits, 
+                                    embedding=HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2"))
+
+retriever = vectorstore.as_retriever()
+
+#### RETRIEVAL and GENERATION ####
+
+# Prompt
+prompt = hub.pull("rlm/rag-prompt")
+
+# LLM
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0)
+
+# Post-processing
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+# Chain
+rag_chain = (
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+
+# Question
+rag_chain.invoke("What is Task Decomposition?")
